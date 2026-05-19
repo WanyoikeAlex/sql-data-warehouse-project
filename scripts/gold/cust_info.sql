cea9853abed978c4062171e8e98a458e1d69da4a
Mellanex <159035805+WanyoikeAlex@users.noreply.github.com>
2026-05-19 20:01:10 +0300

    Update cust_info.sql

--- scripts/gold/cust_info.sql
+++ scripts/gold/cust_info.sql
@@ -1,1 +1,15 @@
-
+CREATE VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country, 
+    ci.cst_marital_status AS marital_status,
+    CASE
+		WHEN  ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+    ci.cst_create_date AS create_date,
+    ca.bdate AS birth_date    
