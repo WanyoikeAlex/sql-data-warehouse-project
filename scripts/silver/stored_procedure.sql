7c9a1301f029c8e26720f553de9af379b0db7b04
Mellanex <159035805+WanyoikeAlex@users.noreply.github.com>
2026-05-19 19:56:44 +0300

    Update stored_procedure.sql

--- scripts/silver/stored_procedure.sql
+++ scripts/silver/stored_procedure.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+CREATE OR REPLACE PROCEDURE silver.load_silver()
 LANGUAGE plpgsql
 AS $$
 DECLARE 
@@ -9,105 +9,185 @@ DECLARE
     duration_seconds NUMERIC;
     batch_duration_seconds NUMERIC;
 BEGIN
-    batch_start_time := clock_timestamp();
     
-    RAISE NOTICE '================================================';
-    RAISE NOTICE 'Loading Bronze Layer';
-    RAISE NOTICE '================================================';
-
-    RAISE NOTICE 'Loading CRM Layer';
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: bronze.crm_cust_info';
+    -- Check for Duplicate
+    batch_start_time := clock_timestamp();
 
     start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_cust_info;
-    RAISE NOTICE '>> Insert Data Into Table: bronze.crm_cust_info';
-    COPY bronze.crm_cust_info(cst_id,cst_key,cst_firstname,cst_lastname,cst_marital_status,cst_gndr,cst_create_date)
-    FROM 'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-    DELIMITER ','
-    CSV HEADER;
+    TRUNCATE TABLE silver.crm_cust_info;
+    INSERT INTO silver.crm_cust_info(cst_id,cst_key,cst_firstname,cst_lastname,cst_marital_status,cst_gndr,cst_create_date)
+
+    SELECT cst_id,cst_key,
+        TRIM(cst_firstname) AS cst_firstname,
+        TRIM(cst_lastname) AS cst_lastname,                 
+        CASE 
+            WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+            WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+            ELSE 'n/a'
+        END AS cst_marital_status
+        ,
+        CASE 
+            WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+            WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+            ELSE 'n/a'
+        END AS cst_gndr    
+        ,cst_create_date
+        
+    FROM (
+        SELECT *, 
+            ROW_NUMBER() OVER (PARTITION BY b.cst_id ORDER BY b.cst_create_date DESC) AS flag_last
+        FROM bronze.crm_cust_info AS b
+        WHERE cst_id IS NOT NULL
+    ) AS ranked
+    WHERE flag_last = 1
+    ;
     end_time := clock_timestamp();
     duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
     RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
 
-    -- SELECT * FROM bronze.crm_cust_info;
 
-    -- CRM PROD INFO
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: crm_prd_info';
+
+    -- Customer Information
     start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_prd_info;
-    COPY bronze.crm_prd_info(prd_id,prd_key,prd_nm,prd_cost,prd_line,prd_start_dt,prd_end_dt)
-    FROM 'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-    DELIMITER ','
-    CSV HEADER;
-    end_time := clock_timestamp();
-    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
-    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
-    -- SELECT * FROM bronze.crm_prd_info;
+    TRUNCATE TABLE silver.crm_prd_info;
+    INSERT INTO silver.crm_prd_info(prd_id,cat_id,prd_key,prd_nm,prd_cost,prd_line,prd_start_dt,prd_end_dt)
 
-    -- ERP SALES----------------------------------------------------------------
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: bronze.crm_sales_details';
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_sales_details;
-    COPY bronze.crm_sales_details(sls_ord_num,sls_prd_key,sls_cust_id,sls_order_dt,sls_ship_dt,sls_due_dt,sls_sales,sls_quantity,sls_price
-    )
-    FROM 'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-    DELIMITER ','
-    CSV HEADER;
-    end_time := clock_timestamp();
-    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
-    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
-    -- SELECT * FROM bronze.crm_sales_details;
+    SELECT prd_id,
+        REPLACE (SUBSTRING(prd_key, 1, 5),'-', '_') AS cat_id,
+        SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key, 
+        prd_nm,
+        COALESCE(prd_cost, 0) AS prd_cost,    
+        CASE
+            WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+            WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+            WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+            WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+            ELSE 'n/a'
+        END AS prd_line
+        ,
 
-    RAISE NOTICE '================================================';
-    RAISE NOTICE 'Loading ERP Layer';
-    RAISE NOTICE '================================================';
+        CAST(prd_start_dt AS DATE) AS prd_start_dt,
+        CAST(LEAD(prd_start_dt) 
+            OVER (PARTITION BY prd_key 
+            ORDER BY prd_start_dt) - 1 AS DATE) 
+            AS prd_end_dt
+    
 
-    -- ERP LOC----------------------------------------------------------------
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: bronze.erp_loc_a101';
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_loc_a101;
-    RAISE WARNING '>> Insert Data Into Table: bronze.erp_loc_a101';
-    COPY bronze.erp_loc_a101(CID,CNTRY)
-    FROM'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-    DELIMITER ','
-    CSV HEADER;
-    end_time := clock_timestamp();
-    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
-    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
-    -- SELECT * FROM bronze.erp_loc_a101;
-
-    -- ERP CUST-----------------------------------------------------------------------------
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: bronze.erp_cust_az12';
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_cust_az12;
-    COPY bronze.erp_cust_az12(CID,BDATE,GEN)
-    FROM'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-    DELIMITER ','
-    CSV HEADER;
-    end_time := clock_timestamp();
-    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
-    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
-    -- SELECT * FROM bronze.erp_cust_az12;
-
-    -- ERP PX CAT------------------------------------------------------------------------------
-    RAISE NOTICE '================================================';
-    RAISE NOTICE '>> Truncate Table: bronze.erp_px_cat_g1v2';
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-    COPY bronze.erp_px_cat_g1v2(ID,CAT,SUBCAT,MAINTENANCE)
-    FROM'C:\temp\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-    DELIMITER ','
-    CSV HEADER;
+    FROM bronze.crm_prd_info;
     end_time := clock_timestamp();
     duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
     RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
 
-        -- SELECT * FROM bronze.erp_px_cat_g1v2;
+    -- CRMS Sales
+    start_time := clock_timestamp();
+    TRUNCATE TABLE silver.crm_sales_details;
+    INSERT INTO silver.crm_sales_details (
+        sls_ord_num, 
+        sls_prd_key, 
+        sls_cust_id, 
+        sls_order_dt, 
+        sls_ship_dt, 
+        sls_due_dt, 
+        sls_sales, 
+        sls_quantity, 
+        sls_price)
+
+    SELECT
+        sls_ord_num , 
+        sls_prd_key , 
+        sls_cust_id ,  
+        CASE 
+            WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) != 8 THEN NULL
+            ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+        END AS sls_order_dt, 
+        CASE 
+            WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) != 8 THEN NULL
+            ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+        END AS sls_ship_dt, 
+        CASE 
+            WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) != 8 THEN NULL
+            ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+        END AS sls_due_dt, 
+        CASE
+            WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+                THEN sls_quantity * ABS(sls_price)
+            ELSE sls_sales
+        END AS sls_sales,
+        sls_quantity, 
+        CASE
+            WHEN sls_price IS NULL OR sls_price <= 0
+                THEN sls_sales / NULLIF(sls_quantity,0)
+            ELSE sls_price
+        END AS sls_price 
+
+    FROM bronze.crm_sales_details;
+    end_time := clock_timestamp();
+    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
+    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
+
+    -- ERP CUSTOMER ID VERIFICATION
+    start_time := clock_timestamp();
+    TRUNCATE TABLE silver.erp_cust_az12;
+    INSERT INTO silver.erp_cust_az12 (cid,
+            bdate, 
+            gen
+        )   
+
+    SELECT  
+        CASE 
+            WHEN cid LIKE 'NAS%' 
+                THEN SUBSTRING(cid, 4,LENGTH(cid)) 
+            ELSE cid
+        END AS cid,
+        CASE
+            WHEN bdate > CURRENT_DATE  
+                THEN NULL
+            ELSE bdate
+        END AS bdate, 
+        CASE
+            WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+            WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+            ELSE 'n/a'
+        END AS gen
+
+    FROM bronze.erp_cust_az12;
+    end_time := clock_timestamp();
+    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
+    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
+
+    -- ERP Silver Data
+    start_time := clock_timestamp();
+    TRUNCATE TABLE silver.erp_loc_a101;
+    INSERT INTO silver.erp_loc_a101 (cid, cntry)
+    SELECT 
+        REPLACE(cid, '-', '') AS cid, 
+        CASE
+            WHEN UPPER(TRIM(cntry)) IN ('US', 'USA', 'UNITED STATES') THEN 'United States'
+            WHEN UPPER(TRIM(cntry)) IN ('DE', 'GERMANY') THEN 'Germany'
+            WHEN TRIM(cntry) IS NULL OR TRIM(cntry) = '' THEN 'n/a'
+            ELSE TRIM(cntry)
+        END AS cntry
+    FROM bronze.erp_loc_a101;
+
+    -- ERP PX
+    start_time := clock_timestamp();
+    TRUNCATE TABLE silver.erp_px_cat_g1v2;
+    INSERT INTO silver.erp_px_cat_g1v2 (
+        id ,
+        cat,
+        subcat, 
+        maintenance)
+    SELECT 
+        id ,
+        cat,
+        subcat, 
+        maintenance
+    FROM bronze.erp_px_cat_g1v2;
+    end_time := clock_timestamp();
+    duration_seconds := EXTRACT(EPOCH FROM (end_time - start_time));
+    RAISE NOTICE '>> Load Duration: % seconds', duration_seconds;
+
+
 
     RAISE NOTICE '================================================';
     batch_end_time := clock_timestamp();
@@ -116,5 +196,7 @@ BEGIN
     EXCEPTION
         WHEN OTHERS THEN
             RAISE NOTICE 'ERROR WHEN LOADING bronze data';
+
+
 END;
 $$;
